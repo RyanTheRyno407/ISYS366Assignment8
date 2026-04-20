a05372306dd80623df953cc36b483007929afd16
Ryan hambrick <ryanhambrick14@gmail.com>
2026-04-20 15:49:25 -0400

    Runs

--- Assignment8/Program.cs
+++ Assignment8/Program.cs
@@ -1,8 +1,18 @@
+using Assignment8.Data;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<Assignment8Context>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Assignment8Context")));
+
+builder.Services.AddScoped<IMovieRepo,MovieRepoEF>();
+
 
 var app = builder.Build();
 
@@ -14,4 +24,4 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-
+app.Run();
