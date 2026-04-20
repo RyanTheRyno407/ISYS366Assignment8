823c5d1ca01a03045339b7d64d52e3f578490c7f
Ryan hambrick <ryanhambrick14@gmail.com>
2026-04-20 15:02:51 -0400

    Missing update method in repo (Might need to change IList and IRepo) Need to add endpoints in endpoint helper as well

---
+++ Assignment8/APIUtils/EndPointHelper.cs
@@ -0,0 +1,95 @@
+﻿using Assignment8.Data;
+using Assignment8.Models;
+
+namespace Assignment8.APIUtils
+{
+    public static class EndPointHelper
+    {
+        public static void MapEndPoints(this WebApplication app)
+        {
+            
+        }
+
+        public static IResult GetAllMoviesAsync(IMovieRepo repo)
+        {
+            return Results.Ok(repo.GetAllAsync());
+        }
+        public static IResult GetMovieByIdAsync(int id, IMovieRepo repo)
+        {
+            try 
+            { 
+                Movie? movie = repo.GetById(id);
+                if (movie != null)
+                {
+                    return Results.Ok(movie);
+                }
+                else
+                {
+                    return Results.NotFound();
+                }
+            }
+            catch (Exception ex)
+            {
+                return Results.Problem(ex.Message);
+            }
+        }
+
+        public static IResult PostMovieAsync( Movie movie, IMovieRepo repo)
+        {
+            try
+            {
+                movie.Id = 0;
+                repo.AddAsync(movie);
+                return Results.Ok();
+            }
+            catch (Exception ex) {
+                return Results.Problem(ex.Message);
+            }
+
+            // AddAsync will be used here from the repo to add a new movie to the database.
+        }
+
+        public static IResult DeleteMovieAsync(int id, IMovieRepo repo)
+        {
+            try
+            {
+                Movie? movie = repo.GetById(id);
+                if (movie != null)
+                {
+                    repo.RemoveAsync(movie);
+                    return Results.Ok();
+                }
+                else
+                {
+                    return Results.NotFound();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return Results.Problem(ex.Message);
+                // RemoveAsync will be used here from the repo to remove a movie from the database.
+            }
+        }
+
+        //public static IResult PutMovieAsync(Movie movie, IMovieRepo repo)
+        //{
+        //    try
+        //    {
+        //        if (repo.(movie))
+        //        {
+        //            return Results.Ok();
+        //        }
+        //        return Results.Problem("Can't update movie");
+        //    }
+        //    catch
+        //    {
+        //        return Results.NotFound();
+        //    }
+
+        //    // UpdateAsync will be used here from the repo to update an existing movie in the database.
+        //}
+
+        
+    }
+}
